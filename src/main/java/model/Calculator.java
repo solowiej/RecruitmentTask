e510921d9c88c89c62275a3f86b5bc7bbf02d344
Piotr Solowiej <solowiej.pio@gmail.com>
2020-05-28 20:57:42 +0200

    small refractor

--- src/main/java/model/Calculator.java
+++ src/main/java/model/Calculator.java
@@ -9,6 +9,11 @@ public class Calculator {
     private int amount;
     private String currencyValue;
 
+    public Calculator(int amount, String currencyType) {
+        this.amount = amount;
+        this.currencyValue = currencyType;
+    }
+
     public double calculate() {
         return amount * Double.valueOf(currencyValue);
     }
